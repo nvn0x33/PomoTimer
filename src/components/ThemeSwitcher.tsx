9cf32d053d97510502f6cf5074087c9600104fdb
nvn0x33 <notnotnaveen@gmail.com>
2026-08-09 18:06:23 +0530

    refactor: separate ThemeSwitcher in different component for readability.

---
+++ src/components/ThemeSwitcher.tsx
@@ -0,0 +1,24 @@
+export default function ThemeSwitcher({
+    accentVars,
+    activeAccent,
+    handleClick,
+}) {
+    return (
+        <div className="flex justify-between">
+            {accentVars.map((accent: string) => {
+                return (
+                    <button
+                        key={accent}
+                        style={{
+                            backgroundColor: `var(--color-${accent})`,
+                        }}
+                        className={`p-4 ${
+                            activeAccent === accent ? "active-theme-button" : ""
+                        }`}
+                        onClick={() => handleClick(accent)}
+                    ></button>
+                );
+            })}
+        </div>
+    );
+}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,39 +1,26 @@
 import { useState } from "react";
+import ThemeSwitcher from "./ThemeSwitcher";
 
+const accentVars: string[] = ["accent1", "accent2", "accent3", "accent4"];
 export default function Header() {
-    const accentVars: string[] = ["accent1", "accent2", "accent3", "accent4"];
+    const [activeAccent, setActiveAccent] = useState<string>(accentVars[0]);
 
-    const [activeAccent, setActiveAccent] = useState(accentVars[0]);
-
-    const handleClick = (e) => {
+    const handleClick = (accent: string) => {
         document.documentElement.style.setProperty(
             `--color-theme`,
-            `var(--color-${e.currentTarget.dataset.var})`
+            `var(--color-${accent})`
         );
-        setActiveAccent(e.currentTarget.dataset.var);
+        setActiveAccent(accent);
     };
 
     return (
         <header className="w-1/2 m-auto max-md:w-full p-2">
             <nav className="flex justify-between p-4 rounded-md bg-white/10">
-                <div className="flex justify-between">
-                    {accentVars.map((accent: string, index: number) => {
-                        const bgClass: string = `bg-${accent}`;
-                        const activeClass: string =
-                            activeAccent === accent
-                                ? "active-theme-button"
-                                : "";
-
-                        return (
-                            <button
-                                key={index + 1}
-                                data-var={accent}
-                                className={`p-4 ${bgClass} ${activeClass}`}
-                                onClick={handleClick}
-                            ></button>
-                        );
-                    })}
-                </div>
+                <ThemeSwitcher
+                    accentVars={accentVars}
+                    activeAccent={activeAccent}
+                    handleClick={handleClick}
+                />
                 {/* It will do something, not sure yet */}
                 <button className="bg-white/20 px-2">Settings</button>
             </nav>
