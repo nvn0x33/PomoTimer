595294a18349cdbee4e08667740a38d42eea6219
nvn0x33 <notnotnaveen@gmail.com>
2026-08-10 12:57:45 +0530

    feat: add start, reset buttons to timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,14 +1,17 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useContext } from "react";
+import { ActiveTask } from "../contexts";
 import { msToMins } from "../utils/unit";
 
+const TABS = [
+    { name: "Pomodoro", defaultMs: 1500000, ms: 1500000 },
+    { name: "Short Break", defaultMs: 300000, ms: 300000 },
+    { name: "Long Break", defaultMs: 900000, ms: 900000 },
+];
 export default function Timer() {
-    const tabNames = [
-        { name: "Pomodoro", timer: "25:00", ms: 1500000 },
-        { name: "Short Break", timer: "5:00", ms: 300000 },
-        { name: "Long Break", timer: "15:00", ms: 900000 },
-    ];
+    const [activeTab, setActiveTab] = useState(TABS[0]);
+    const [isTimerRunning, setIsTimerRunning] = useState(false);
 
-    const [activeTab, setActiveTab] = useState(tabNames[0]);
+    const Task: string = useContext(ActiveTask);
     const intervalID = useRef(null);
 
     const startTimer = () => {
@@ -20,19 +23,42 @@ export default function Timer() {
                 return { ...temp };
             });
         }, 1000);
+        setIsTimerRunning(true);
     };
     const stopTimer = () => {
         clearInterval(intervalID.current);
         intervalID.current = null;
+        setIsTimerRunning(false);
+    };
+
+    const toggleTimer = () => {
+        if (isTimerRunning) {
+            stopTimer();
+        } else {
+            startTimer();
+        }
+    };
+
+    const resetTimer = () => {
+        stopTimer();
+        setActiveTab((prev) => {
+            const temp = { ...prev };
+            temp.ms = temp.defaultMs;
+
+            return { ...temp };
+        });
     };
 
     return (
         <div className="flex flex-col gap-12 bg-white/10 rounded-md px-16 py-4 text-center">
             <div className="flex gap-4">
-                {tabNames.map((tab) => (
+                {TABS.map((tab) => (
                     <button
                         key={tab.name}
                         onClick={() => {
+                            if (tab.name === activeTab.name) return;
+
+                            stopTimer();
                             setActiveTab(tab);
                         }}
                         className={`px-2 py-1 rounded-sm ${
@@ -46,17 +72,27 @@ export default function Timer() {
                 ))}
             </div>
 
-            <span className="font-extrabold text-8xl text-center">
+            <span className="font-extrabold text-8xl">
                 {msToMins(activeTab.ms)}
             </span>
             <div className="flex justify-between">
-                <button
-                    onClick={startTimer}
-                    className="bg-white px-8 py-2 text-theme font-bold text-lg"
-                >
-                    START
-                </button>
+                <Button
+                    text={isTimerRunning ? "PAUSE" : "START"}
+                    handleClick={toggleTimer}
+                />
+                <Button text={"RESET"} handleClick={resetTimer} />
             </div>
         </div>
+    );
+}
+
+function Button({ text, handleClick }) {
+    return (
+        <button
+            onClick={handleClick}
+            className="bg-white px-8 py-2 text-theme font-bold text-lg"
+        >
+            {text}
+        </button>
     );
 }
